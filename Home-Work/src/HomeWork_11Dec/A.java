07bc12ec387f4ddbeb594299a0f71ae190694c59
Ambalkar <devendraambalkar11@gmail.com>
2025-12-28 07:58:52 +0530

    Home-work 11 Dec 2025

--- Home-Work/src/HomeWork_11Dec/A.java
+++ Home-Work/src/HomeWork_11Dec/A.java
@@ -1,3 +1,20 @@
+// package HomeWork_11Dec;
+
+// public class A {
+// 	int b = 20;
+// 	void m1() {
+// 		int c = 78;
+// 		System.out.println(b);
+// 	}
+// 	void m2() {
+// 		System.out.println(b);
+
+// 		System.out.println(c);
+// 	}
+// }
+
+
+//sollution:
 package HomeWork_11Dec;
 
 public class A {
@@ -5,8 +22,9 @@ public class A {
 	void m1() {
 		int c = 78;
 		System.out.println(b);
+		m2(c);
 	}
-	void m2() {
+	void m2(int c) {
 		System.out.println(b);
 
 		System.out.println(c);
